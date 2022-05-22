cb7bb2c6598d5801896d26093f286eb279d4ada9
CoderRaunak <75471830+GamerRaunak@users.noreply.github.com>
2022-05-22 23:54:45 +0530

    Add files via upload

---
+++ overload_vol.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class overload_vol
+{
+    double volume(double R)
+    {
+        double V=(4/3.0)*(22/7.0)*Math.pow(R,3);
+        return V;
+    }
+    double volume(double H,double R)
+    {
+        double V=(22/7.0)*R*R*H;
+        return V;
+    }
+    double volume(double L,double B,double H)
+    {
+        double V=L*B*H;
+        return V;
+    }
+}
